8e9234545dffd20bb1b9c839e727979b3fc4e9ed
MahmoodFalmaz <falmaz@ualberta.ca>
2020-10-27 00:50:31 +0300

    Created a login and sign up activites
    feat: created the login and sign up layouts
    feat: connected the app to the database this include reading and writing to the database
    feat: The user could login or sign up to the app and the user data will be added to the database
    feat: The user information are validated when they login or sign up, the database is also checked
    feat: Added the user attributes to the user class and created constructors, setters, and getters
    fix: commented out parts of the MainActivity for now

---
+++ app/src/main/java/com/example/pocketbook/activity/Login.java
@@ -0,0 +1,17 @@
+package com.example.pocketbook.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pocketbook.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
--- app/src/main/java/com/example/pocketbook/model/User.java
+++ app/src/main/java/com/example/pocketbook/model/User.java
@@ -1,26 +1,79 @@
 package com.example.pocketbook.model;
 
 public class User {
-    private String username;
-    private String first_name;
-    private String last_name;
+
+    private String fullName;
     private String email;
+    private String username;
+    private String password;
     private String photo;
 
     public User() {} // used by firestore to automatically create new object
 
-    public User(String username, String first_name, String last_name,
-                String email, String photo) {
+    public User(String fullName, String email, String username) {
+        this.fullName = fullName;
+        this.email = email;
         this.username = username;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email ;
+    }
+
+    public User(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public User(String fullName, String email, String username, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String fullName, String email, String username, String password, String photo) {
+        this.fullName = fullName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
         this.photo = photo;
     }
 
-    public String getUsername() { return username; }
-    public String getFirst_name() { return first_name; }
-    public String getLast_name() { return last_name; }
-    public String getEmail() { return email; }
-    public String getPhoto() { return photo; }
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
 }
+

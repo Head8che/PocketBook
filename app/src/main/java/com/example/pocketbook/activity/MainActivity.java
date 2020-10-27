46bd96e93788baf35e9ac07ba8c1e32abc9f09b4
Mahmoud Abu Jayyab <abujayya@ualberta.ca>
2020-10-27 15:13:13 -0600

    feat: added ViewBookActivity and a corresponding xml file fix: fixed issue in main activity that prevented the app from running added a temporary default user profile image asset

--- app/src/main/java/com/example/pocketbook/activity/MainActivity.java
+++ app/src/main/java/com/example/pocketbook/activity/MainActivity.java
@@ -1,39 +1,24 @@
 package com.example.pocketbook.activity;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.pocketbook.R;
-import com.example.pocketbook.adapter.BookAdapter;
 import com.example.pocketbook.model.Book;
-
-//import com.example.pocketbook.model.User;
-
-import com.example.pocketbook.model.User;
+import com.example.pocketbook.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 
 import android.content.Intent;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -41,17 +26,6 @@ import android.widget.Toast;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-
-//    private static final int numColumns = 2;
-//    private static final int LIMIT = 20;
-//
-//    private FirebaseFirestore mFirestore;
-//    private Query mQuery;
-//
-//    private RecyclerView mBooksRecycler;
-//    private BookAdapter mAdapter;
-//
-//    Button viewMyBookBtn;
 
     private FirebaseAuth mAuth;
     private Button signUp, login, forgotPass;
@@ -76,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), ViewMyBookActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ViewBookActivity.class);
                 // the book UID will actually be gotten onClick, but it's hardcoded for now
                 intent.putExtra("FIRESTORE_BOOK_UID", "fBtIHykc3KqxgPmwlNYO");
                 startActivity(intent);
@@ -84,28 +58,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Initialize Firestore and main RecyclerView
-        initFirestore();
-//        generateData();
-        initRecyclerView();
-    }
+//        initFirestore();
+////        generateData();
+//        initRecyclerView();
 
-                String email = userEmail.getText().toString().trim();
-                String password = userPassword.getText().toString().trim();
-                if(email.isEmpty()){
-                    userEmail.setError("Need an email!");
-                    userEmail.requestFocus();
-                    return;
-                }
-
-                if(password.isEmpty()){
-                    userPassword.setError("Password Required");
-                    userPassword.requestFocus();
-                    return;
-                }
-                Register(email,password);
-
-            }
-        });
+        String email = userEmail.getText().toString().trim();
+        String password = userPassword.getText().toString().trim();
+        if (email.isEmpty()) {
+            userEmail.setError("Need an email!");
+            userEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            userPassword.setError("Password Required");
+            userPassword.requestFocus();
+            return;
+        }
+        Register(email, password);
 
 
         signUp.setOnClickListener(new View.OnClickListener() {
@@ -115,63 +84,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-    private void generateData(){
-        // for demonstration purposes
-        for(int i=1; i<7; i++) {
-            String uniqueID = UUID.randomUUID().toString();
-//            User owner = new User("userEden");
-//            Book book = new Book(uniqueID, "Book "+i, "LeFabulous", uniqueID,  owner, "none", "available","none");
-            Book book = new Book(uniqueID, "Book "+i, "LeFabulous", uniqueID, "eden", "none", "available","none");
-            mFirestore.collection("books").document(book.getId()).set(book);
-        }
     }
 
-//        viewMyBookBtn = findViewById(R.id.viewMyBookBtn);
-//
-//        viewMyBookBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), ViewMyBookActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-
-//        // Initialize Firestore and main RecyclerView
-//        initFirestore();
-////        generateData();
-//        initRecyclerView();
-//    }
-//
-//    private void initFirestore(){
-//        mFirestore = FirebaseFirestore.getInstance();
-//
-//        // Query to retrieve all books
-//        mQuery = mFirestore.collection("books")
-//                .limit(LIMIT);
-//    }
-//
-//    private void initRecyclerView() {
-//        if (mQuery == null)
-//            Log.d(TAG, "No query, not initializing RecyclerView");
-//
-//        mAdapter = new BookAdapter(mQuery){
-//            @Override
-//            protected void onDataChanged() {
-//                // Show/hide content if the query returns empty.
-//                if (getItemCount() == 0)
-//                    mBooksRecycler.setVisibility(View.GONE);
-//                else
-//                    mBooksRecycler.setVisibility(View.VISIBLE);
-//            }
-//        };
-//
-//        // use a linear layout manager
-//        mBooksRecycler.setLayoutManager(new GridLayoutManager(this, numColumns));
-//        mBooksRecycler.setAdapter(mAdapter);
-//    }
-//
 //    private void generateData(){
 //        // for demonstration purposes
 //        for(int i=1; i<7; i++) {
@@ -180,67 +94,7 @@ public class MainActivity extends AppCompatActivity {
 //            mFirestore.collection("books").document(book.getId()).set(book);
 //        }
 //    }
-//
-//    private void retrieveData() {
-//        // for demonstration purposes
-//        // query to retrieve all books
-//        Query query = mFirestore.collection("books");
-//
-//        // Getting data once
-////        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-////            @Override
-////            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-////                if(task.isSuccessful()){
-////                    for(QueryDocumentSnapshot document : task.getResult())
-////                        Log.d(TAG, document.getId() + " => " + document.getData());
-////                } else
-////                    Log.d(TAG, "Error getting documents: ", task.getException());
-////            }
-////        });
-//
-//        // Getting data real time
-//        // listens to multiple documents
-//        // differentiates what kind of changes happened
-//        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot snapshot, @Nullable FirebaseFirestoreException e) {
-//                if (e != null) {
-//                    Log.w(TAG, "Listen failed.", e);
-//                    return;
-//                }
-//                for (DocumentChange dc : snapshot.getDocumentChanges()){
-//                    switch (dc.getType()) {
-//                        case ADDED:
-//                            Log.d(TAG, "New book: " + dc.getDocument().getData());
-//                            break;
-//                        case MODIFIED:
-//                            Log.d(TAG, "Modified book: " + dc.getDocument().getData());
-//                            break;
-//                        case REMOVED:
-//                            Log.d(TAG, "Removed book: " + dc.getDocument().getData());
-//                            break;
-//                    }
-//                }
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // Start listening for Firestore updates
-//        if (mAdapter != null) {
-//            mAdapter.startListening();
-//        }
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        if (mAdapter != null) {
-//            mAdapter.stopListening();
-//        }
-    }
+
 
     public void Register(String email, String password){
         mAuth.signInWithEmailAndPassword(email, password)
@@ -250,23 +104,17 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
-                            Toast.makeText(MainActivity.this, "Welcome to Pocketbook.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Welcome to Pocketbook.",Toast.LENGTH_SHORT).show();
                             FirebaseUser user = mAuth.getCurrentUser();
                             Intent intent = new Intent(getApplicationContext(), ViewMyBookActivity.class);
                             startActivity(intent);
-//                            updateUI(user);
-                        } else {
+                        }
+                        else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Failed to Login.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Failed to Login.", Toast.LENGTH_SHORT).show();
                         }
-
-                        // ...
                     }
                 });
-
     }
-
 }
---
+++ app/src/main/java/com/example/pocketbook/activity/ViewBookActivity.java
@@ -0,0 +1,17 @@
+package com.example.pocketbook.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pocketbook.R;
+
+public class ViewBookActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_book);
+        // in progress
+    }
+}

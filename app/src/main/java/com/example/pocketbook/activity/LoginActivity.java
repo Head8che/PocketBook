462318f2d3c89ba2fe8c770d2dbd7915aa493f22
MahmoodFalmaz <falmaz@ualberta.ca>
2020-10-29 02:49:06 +0300

    Edited the user class and sign up class
    refactor: added the first & last name fields to the sign up page
    refactor: added the first & last name fields to the user class and created new getters and setters

--- app/src/main/java/com/example/pocketbook/activity/LoginActivity.java
+++ app/src/main/java/com/example/pocketbook/activity/LoginActivity.java
@@ -78,9 +78,7 @@ public class LoginActivity extends AppCompatActivity {
                                     Toast.LENGTH_SHORT).show();
                             FirebaseUser user = mAuth.getCurrentUser();
                             Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-//                            intent.putExtra('username')
                             startActivity(intent);
-//                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());

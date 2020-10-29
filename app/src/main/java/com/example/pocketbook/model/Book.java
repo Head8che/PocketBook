7cd2059d984c11b9c5d9b05d10d860abead2f75b
Della <humanita.1999@gmail.com>
2020-10-28 20:53:24 -0600

    fix: Integrated Parser into Book model

--- app/src/main/java/com/example/pocketbook/model/Book.java
+++ app/src/main/java/com/example/pocketbook/model/Book.java
@@ -1,12 +1,10 @@
 package com.example.pocketbook.model;
 
-import com.example.pocketbook.util.Parser;
-
-public class Book {
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.pocketbook.util.Parser;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -16,6 +14,7 @@ import com.google.firebase.storage.StorageReference;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class Book implements Serializable {
     private String id;
@@ -30,59 +29,6 @@ public class Book implements Serializable {
 
     Parser parser;
 
-    public Book() {} // used by firestore to populate a book
-
-    public Book(String title, String author, String ISBN) {
-        // validates the arguments
-        parser = new Parser(title, author, ISBN);
-
-        if (parser.checkTitleAndAuthor()) {
-            this.title = title;
-            this.author = author;
-        }
-        if (parser.checkIsbn()) {
-            this.ISBN = ISBN;
-        }
-    }
-
-    public Book(String title, String author, String ISBN, String owner,
-                String comment, String status, String photo) {
-        // validates the arguments
-        parser = new Parser(title, author, ISBN, comment);
-        if (parser.checkTitleAndAuthor()) {
-            this.title = title;
-            this.author = author;
-        }
-        if (parser.checkIsbn()) {
-            this.ISBN = ISBN;
-        }
-
-        if (parser.checkComment()) {
-            this.comment = comment;
-        }
-
-        this.owner = owner;
-        this.status = status;
-        this.photo = photo;
-    }
-
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public String getISBN() { return ISBN; }
-    public String getOwner() { return owner; }
-    public String getComment() { return comment; }
-    public String getStatus() { return status; }
-    public String getPhoto() { return photo; }
-
-    public void setTitle(String title) { this.title = title; }
-    public void setAuthor(String author) { this.author = author; }
-    public void setISBN(String ISBN) { this.ISBN = ISBN; }
-    public void setOwner(String owner) { this.owner = owner; }
-    public void setComment(String comment) { this.comment = comment; }
-    public void setStatus(String status) { this.status = status; }
-    public void setPhoto(String photo) { this.photo = photo; }
-
-}
     /**
      * Firestore constructor
      *  to populate a book
@@ -99,12 +45,31 @@ public class Book implements Serializable {
      * @param status : indicates availability of book (available, requested, accepted, borrowed)
      */
     public Book(String id, String title, String author, String isbn, String owner, String status) {
+        // validates the input before setting them
+        parser = new Parser(title, author, isbn);
+
         this.id = id.trim();
-        this.title = title.trim();
-        this.author = author.trim();
-        this.isbn = isbn.trim();
+
+        if (parser.checkTitleAndAuthor()) {
+            this.title = title.trim();
+            this.author = author.trim();
+        }
+        else {
+            this.title = "No Title";
+            this.author = "No Author";
+        }
+
+        if (parser.checkIsbn()) {
+            this.isbn = isbn.trim();
+        }
+        else {
+            this.isbn = "N/A";
+        }
         this.owner = owner.trim();
         this.status = status.trim();
+
+
+
     }
 
     /**

35b807a00b069052dc392f81569c509136194083
Della <humanita.1999@gmail.com>
2020-10-29 14:12:33 -0600

    fix: added book as an arg to Book for AddBook functionality. Created a constructor without id for the purpose of adding books.

--- app/src/main/java/com/example/pocketbook/model/Book.java
+++ app/src/main/java/com/example/pocketbook/model/Book.java
@@ -48,8 +48,6 @@ public class Book implements Serializable {
         // validates the input before setting them
         Parser parser = new Parser(title, author, isbn);
 
-        this.id = "tempId";
-
         if (parser.checkTitleAndAuthor()) {
             this.title = title.trim();
             this.author = author.trim();
@@ -162,9 +160,8 @@ public class Book implements Serializable {
         ownedBooks list with a new or updated bookID.
      */
     /* Setter Functions */
-    public void editBook(String id, String title, String author, String isbn, String owner,
+    public void setBook(String title, String author, String isbn, String owner,
                         String status, String comment, String condition, String photo) {
-        this.id = id.trim();
         this.title = title.trim();
         this.author = author.trim();
         this.isbn = isbn.trim();
@@ -178,7 +175,6 @@ public class Book implements Serializable {
                 ? null : photo.trim();
 
         Map<String, Object> docData = new HashMap<>();
-        docData.put("id", this.id);
         docData.put("title", this.title);
         docData.put("author", this.author);
         docData.put("isbn", this.isbn);

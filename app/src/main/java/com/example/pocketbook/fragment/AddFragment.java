32372d5fc689df15029959c168a3487f2ca6a0d8
Della <humanita.1999@gmail.com>
2020-10-28 22:42:49 -0600

    feat: added imageButton. pending: transfer data between AddBook and ViewMyBook fragments

--- app/src/main/java/com/example/pocketbook/fragment/AddFragment.java
+++ app/src/main/java/com/example/pocketbook/fragment/AddFragment.java
@@ -6,12 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.pocketbook.R;
+import com.example.pocketbook.model.Book;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddFragment extends Fragment {
@@ -21,11 +26,14 @@ public class AddFragment extends Fragment {
     private EditText isbnText;
     private EditText commentText;
     private Button addButton;
-    private FirebaseFirestore mFirestore;
+    private ImageButton imageButton;
+
+    public FirebaseUser owner;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        owner = FirebaseAuth.getInstance().getCurrentUser();
         return inflater.inflate(R.layout.fragment_add, container, false);
     }
 
@@ -39,7 +47,10 @@ public class AddFragment extends Fragment {
         isbnText = (EditText) view.findViewById(R.id.editText_isbn);
         commentText = (EditText) view.findViewById(R.id.editText_comment);
 
-        // get the button and create the listener
+        /**
+         * Add Book Handler
+         */
+        // get the ADD button and create the listener
         addButton = view.findViewById(R.id.button_addBook);
         addButton.setOnClickListener(new View.OnClickListener() {
 
@@ -47,13 +58,49 @@ public class AddFragment extends Fragment {
             final String title = titleText.getText().toString();
             final String isbn = isbnText.getText().toString();
             final String comment = commentText.getText().toString();
+            // all new books to be added are available
+            final String status = "available";
 
+            // get the user credentials
+            final FirebaseUser owner = FirebaseAuth.getInstance().getCurrentUser();
+            final String ownerId = owner.getUid();
+
+            // TODO:  how can an id be created before the book has been added?
+            ////// TEMP SOLUTION
+            final String bookId = "tempId";
 
             @Override
             public void onClick(View view) {
                 // create Book object and add to Firestore
+                Book book = new Book(bookId, title, author, isbn, ownerId, status);
+                book.setBook(bookId, title, author, isbn, ownerId, status,
+                        null, null, null);
+                // go to the view owned book activity
+            }
 
+        });
+
+        /**
+         * Add Image Handler
+         *
+         * Resource :
+         * https://stackoverflow.com/questions/9107900/how-to-upload-image-from-gallery-in-android
+         * https://medium.com/@hasangi/capture-image-or-choose-from-gallery-photos-implementation-for-android-a5ca59bc6883
+         */
+        imageButton = view.findViewById(R.id.image_button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ///////////// IN PROGRESS
+                // create a dialogue fragment that allows
+                //  - take photo
+                //  - choose from gallery
+                //  - cancel
+                // retrieve the image
+                // add to Firestore
+                // set image as image uploaded as imageView
             }
         });
+
     }
 }

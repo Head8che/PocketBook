605b63e067e6986a92395e3bc73aa05d9abc15db
Ayo Akindele <a.akindele01@gmail.com>
2020-11-03 12:59:44 -0700

    refactor: updated EditBookActivity layout

--- app/src/main/java/com/example/pocketbook/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/pocketbook/activity/SplashScreenActivity.java
@@ -1,15 +1,27 @@
 package com.example.pocketbook.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.example.pocketbook.R;
+import com.example.pocketbook.model.User;
+import com.example.pocketbook.util.FirebaseIntegrity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Locale;
 import java.util.Objects;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -25,15 +37,38 @@ public class SplashScreenActivity extends AppCompatActivity {
          FirebaseAuth.getInstance().signOut();
 
         /* Duration of wait in milliseconds */
-        int SPLASH_DISPLAY_LENGTH = 3000;
+        int SPLASH_DISPLAY_LENGTH = 1500;
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 /* keep intent in condition because auth could be async */
                 if (FirebaseAuth.getInstance().getCurrentUser() != null) {
                     mainIntent = new Intent(getApplicationContext(), HomeActivity.class);
-                    SplashScreenActivity.this.startActivity(mainIntent);
-                    SplashScreenActivity.this.finish();
+
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                    DocumentReference docRef = FirebaseFirestore.getInstance()
+                            .collection("users").document(Objects.requireNonNull(user.getEmail()));
+
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    User currentUser = FirebaseIntegrity.getUserFromFirestore(document);
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    intent.putExtra("CURRENT_USER", currentUser);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Log.d("SPLASH", "No such document");
+                                }
+                            } else {
+                                Log.d("SPLASH", "get failed with ", task.getException());
+                            }
+                        }
+                    });
                 } else {
                     mainIntent = new Intent(getApplicationContext(), LoginActivity.class);
                     SplashScreenActivity.this.startActivity(mainIntent);

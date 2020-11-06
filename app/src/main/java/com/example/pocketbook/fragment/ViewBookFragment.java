b4580afe573640697c40903c9fcd0da1e038492e
Mahmoud Abu Jayyab <abujayya@ualberta.ca>
2020-11-05 18:53:36 -0700

    doc: added documentation to ViewBookFragment

--- app/src/main/java/com/example/pocketbook/fragment/ViewBookFragment.java
+++ app/src/main/java/com/example/pocketbook/fragment/ViewBookFragment.java
@@ -54,14 +54,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ViewBookFragment extends androidx.fragment.app.Fragment {
 
-    private FirebaseFirestore db;
-    private FirebaseAuth mAuth;
-
     private Book book;
     private User currentUser;
     private User bookOwner;
 
-    private ImageView userProfile;
+    private CircleImageView userProfilePicture;
     private ImageView bookCoverImageView;
     private ImageView bookStatusImage;
     private TextView bookTitleField;
@@ -71,14 +68,13 @@ public class ViewBookFragment extends androidx.fragment.app.Fragment {
     private TextView conditionField;
     private Button requestButton;
 
-    private String bookId;
-    private String bookTitle;
-    private String bookAuthor;
-    private String bookComment;
-    private String bookIsbn;
-    private String bookCondition;
-    private String bookStatus;
-    private StorageReference bookCover;
+    /**
+     * create a new instance of the ViewBookFragment
+     * @param currentUser: the user currently signed in the app
+     * @param bookOwner: the owner of the book being viewed
+     * @param book: the book being viewed
+     * @return
+     */
 
     public static ViewBookFragment newInstance(User currentUser, User bookOwner, Book book) {
         ViewBookFragment viewBookFragment = new ViewBookFragment();
@@ -94,69 +90,70 @@ public class ViewBookFragment extends androidx.fragment.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //get the arguments passed to the fragment as a bundle
         if (getArguments() != null) {
             this.currentUser = (User) getArguments().getSerializable("BA_CURRENTUSER");
             this.book = (Book) getArguments().getSerializable("BA_BOOK");
             this.bookOwner = (User) getArguments().getSerializable("BA_BOOKOWNER");
-            //Log.d("debugkjwewwee",bookOwner.toString());
         }
-
-        bookId = book.getId();
-        bookTitle = book.getTitle();
-        bookAuthor = book.getAuthor();
-        bookComment = book.getComment();
-        bookIsbn = book.getISBN();
-        bookCondition = book.getCondition();
-        bookStatus =  book.getStatus();
-        bookCover = book.getBookCover();
-        db = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //clear the container before starting the fragment
         if (container != null) {
             container.removeAllViews();
         }
+        //inflate fragment in container
         View view = inflater.inflate(R.layout.fragment_view_book, container, true);
 
-        TextView bookTitleField = view.findViewById(R.id.viewBookTitle);
-        TextView bookAuthorField = view.findViewById(R.id.viewBookAuthor);
-        TextView isbnField = view.findViewById(R.id.viewBookISBN);
-        TextView conditionField = view.findViewById(R.id.viewBookCondition);
-        TextView commentField = view.findViewById(R.id.viewBookComment);
+        //get the views' ids
+        bookTitleField = view.findViewById(R.id.viewBookTitle);
+        bookAuthorField = view.findViewById(R.id.viewBookAuthor);
+        isbnField = view.findViewById(R.id.viewBookISBN);
+        conditionField = view.findViewById(R.id.viewBookCondition);
+        commentField = view.findViewById(R.id.viewBookComment);
+        requestButton = view.findViewById(R.id.viewBookRequestBtn);
+        userProfilePicture = view.findViewById(R.id.viewBookUserProfile);
+        bookCoverImageView = view.findViewById(R.id.bookCover);
+        bookStatusImage = (ImageView) view.findViewById(R.id.viewBookBookStatusImageView);
 
-        Button requestButton = view.findViewById(R.id.viewBookRequestBtn);
-        CircleImageView userProfilePicture = view.findViewById(R.id.viewBookUserProfile);
-        ImageView bookCoverImageView = view.findViewById(R.id.bookCover);
-        ImageView bookStatusImage = (ImageView) view.findViewById(R.id.viewBookBookStatusImageView);
-
-        bookTitleField.setText(bookTitle);
-        bookAuthorField.setText(bookAuthor);
-
-        isbnField.setText(getResources().getString(R.string.isbn_text, bookIsbn));
-
-
+        //set the views' values to the values of the book being viewed
+        bookTitleField.setText(book.getTitle());
+        bookAuthorField.setText(book.getAuthor());
+        isbnField.setText(getResources().getString(R.string.isbn_text, book.getISBN()));
+        //display a comment if the owner optionally added one to the book
+        if (book.getComment() != null) {
+            commentField.setText(getResources().getString(R.string.comment_text,
+                    book.getComment()));
+        } else {
+            commentField.setVisibility(View.GONE);
+        }
+        //display the book's condition if the owner optionally added one to the book
+        if (book.getCondition() != null) {
+            conditionField.setText(getResources().getString(R.string.condition_text,
+                    book.getCondition()));
+        } else {
+            conditionField.setVisibility(View.GONE);
+        }
         GlideApp.with(Objects.requireNonNull(getContext()))
-                .load(bookCover)
+                .load(book.getBookCover())
                 .into(bookCoverImageView);
 
+        //get the profile picture of the book's owner and set it in its field
         GlideApp.with(Objects.requireNonNull(getContext()))
                 .load(bookOwner.getProfilePicture())
                 .circleCrop()
                 .into(userProfilePicture);
 
-        boolean available;
-        switch(bookStatus) {
+        //boolean to track if a book is available for being requested
+        boolean available = false;
+
+        switch(book.getStatus()) {
+            //if the book is borrowed or accepted by another user, it is not available for requesting
             case "BORROWED":
-                requestButton.setClickable(false);
-                requestButton.setText("Not Available");
-                requestButton.setBackgroundColor(getResources().getColor(R.color.notAvailable));
-                bookStatusImage.setImageResource(R.drawable.ic_borrowed);
-                bookStatusImage.setColorFilter(ContextCompat.getColor(getContext(), R.color.colorBorrowed),android.graphics.PorterDuff.Mode.SRC_IN);
-                available = false;
-                break;
             case "ACCEPTED":
                 requestButton.setClickable(false);
                 requestButton.setText("Not Available");
@@ -165,7 +162,9 @@ public class ViewBookFragment extends androidx.fragment.app.Fragment {
                 bookStatusImage.setColorFilter(ContextCompat.getColor(getContext(), R.color.colorBorrowed),android.graphics.PorterDuff.Mode.SRC_IN);
                 available = false;
                 break;
+
             case "REQUESTED":
+                //if the book has been requested by this user before, it is not available for requesting again
                 if (book.getRequestList().containsRequest(currentUser.getEmail())){
                     requestButton.setText("Already Requested!");
                     requestButton.setBackgroundColor(getResources().getColor(R.color.notAvailable));
@@ -173,40 +172,41 @@ public class ViewBookFragment extends androidx.fragment.app.Fragment {
                     bookStatusImage.setColorFilter(ContextCompat.getColor(getContext(), R.color.colorBorrowed),android.graphics.PorterDuff.Mode.SRC_IN);
                     available = false;
                 }
+                //if the book has no requests, it is available for requesting
+                else if (book.getRequestList().getSize() == 0){
+                    book.setStatus("AVAILABLE");
+                    available =  true;
+                }
+                break;
+
+            //default case when the book is available
             default:
                 bookStatusImage.setImageResource(R.drawable.ic_available);
                 bookStatusImage.setColorFilter(ContextCompat.getColor(getContext(), R.color.colorAvailable), android.graphics.PorterDuff.Mode.SRC_IN);
                 available = true;
         }
 
-        if (bookComment != null) {
-            commentField.setText(getResources().getString(R.string.comment_text,
-                    bookComment));
-        } else {
-            commentField.setVisibility(View.GONE);
-        }
-
-        if (bookCondition != null) {
-            conditionField.setText(getResources().getString(R.string.condition_text,
-                    bookCondition));
-        } else {
-            conditionField.setVisibility(View.GONE);
-        }
-
+        // if the book is available for requesting, the user can tap the request button to request the book
         if (available) {
             requestButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
+                    //add a pending request to the book's requestList
                     book.addRequest(new Request(currentUser.getEmail(), bookOwner.getEmail(), book));
+
+                    //display a message confirming that the book has been requested
                     Toast.makeText(getActivity(), String.format(Locale.CANADA,
                             "You have requested %s!",book.getTitle()),
                             Toast.LENGTH_SHORT).show();
 
+                    //go back to the previous fragment
                     getActivity().onBackPressed();
                 }
             });
         }
-        // return view
+
+        //return the view
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 

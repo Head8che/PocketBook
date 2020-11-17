1fe80442e742aa47cdf6f7736951f648286b42b2
Head8che <hagi@ualberta.ca>
2020-11-17 03:50:03 -0700

    Calender Dialog

--- app/src/main/java/com/example/pocketbook/fragment/SetLocationFragment.java
+++ app/src/main/java/com/example/pocketbook/fragment/SetLocationFragment.java
@@ -1,15 +1,26 @@
 package com.example.pocketbook.fragment;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,6 +34,10 @@ import com.example.pocketbook.model.User;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class SetLocationFragment extends Fragment {
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +45,23 @@ public class SetLocationFragment extends Fragment {
     private RequestAdapter requestAdapter;
     private Book book;
     private User currentUser;
-    Button setLocation;
+    TextInputEditText setLocation;
+    TextInputEditText setDate;
+    TextInputEditText setTime;
+    String selectedDate;
+    public static final int REQUEST_CODE = 11; // Used to identify the result
+    final Calendar myCalendar = Calendar.getInstance();
+    private TimePickerDialog timePickerDialog;
 
 
 
     public SetLocationFragment() {
         // Required empty public constructor
+    }
+
+    public static SetLocationFragment newInstance() {
+        SetLocationFragment fragment = new SetLocationFragment();
+        return fragment;
     }
 
 
@@ -44,13 +70,45 @@ public class SetLocationFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_set_location, container, false);
         ImageView backButton = (ImageView) view.findViewById(R.id.setLocationBackBtn);
-        setLocation = (Button) view.findViewById(R.id.confirmPickupBtn);
+        setLocation = (TextInputEditText) view.findViewById(R.id.setPickup);
+        setDate = (TextInputEditText) view.findViewById(R.id.setDate);
+        setTime = (TextInputEditText) view.findViewById(R.id.setTime);
+
+
+
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        setDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(getActivity(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        final FragmentManager fm = ((AppCompatActivity)getActivity()).getSupportFragmentManager();
+
+
 
         setLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,15 +123,20 @@ public class SetLocationFragment extends Fragment {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("USER","USERRRR");
                 getActivity().onBackPressed();
             }
         });
 
-
-
         return view;
     }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        setDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
 }
 
 
